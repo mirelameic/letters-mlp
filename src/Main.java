55197fc16a53f005f7ccd3c1641084dc52cd8285
mirelameic <mirelameic4@gmail.com>
2024-04-24 23:43:02 -0300

    fix main comments

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main{
     public static void main(String[] args){
-        /* 2 neurônios de entrada, 3 neurônios ocultos e 1 neurônio de saída */
+        /* 2 neurônios de entrada, 5 neurônios ocultos e 2 neurônios de saída */
         int[] layerSizes = {2, 5, 2};
         NeuralNetwork neuralNetwork = new NeuralNetwork(layerSizes);
         System.out.println(neuralNetwork);
 
-        /* 1 dado de entrada = 0.5 */
+        /* 2 dados de entrada = 0.5, 1.0 */
         double[] inputs = {0.5, 1};
         double[] outputs = neuralNetwork.getOutputs(inputs);
         
