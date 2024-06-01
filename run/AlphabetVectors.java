177d669d134cf8301f014ba113efb3782c1e8414
mirelameic <mirelameic4@gmail.com>
2024-06-01 20:16:14 -0300

    wip trainning

---
+++ run/AlphabetVectors.java
@@ -0,0 +1,88 @@
+public class AlphabetVectors{
+    public static final double[] A = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] B = {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] C = {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] D = {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] E = {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] F = {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] G = {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] H = {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] I = {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] J = {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] K = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] L = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] M = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] N = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] O = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] P = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] Q = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] R = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] S = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0};
+    public static final double[] T = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0};
+    public static final double[] U = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0};
+    public static final double[] V = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0};
+    public static final double[] W = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0};
+    public static final double[] X = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0};
+    public static final double[] Y = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0};
+    public static final double[] Z = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
+
+    public static double[] getLetter(int linha){
+        switch (linha){
+            case 1:
+                return A;
+            case 2:
+                return B;
+            case 3:
+                return C;
+            case 4:
+                return D;
+            case 5:
+                return E;
+            case 6:
+                return F;
+            case 7:
+                return G;
+            case 8:
+                return H;
+            case 9:
+                return I;
+            case 10:
+                return J;
+            case 11:
+                return K;
+            case 12:
+                return L;
+            case 13:
+                return M;
+            case 14:
+                return N;
+            case 15:
+                return O;
+            case 16:
+                return P;
+            case 17:
+                return Q;
+            case 18:
+                return R;
+            case 19:
+                return S;
+            case 20:
+                return T;
+            case 21:
+                return U;
+            case 22:
+                return V;
+            case 23:
+                return W;
+            case 24:
+                return X;
+            case 25:
+                return Y;
+            case 26:
+                return Z;
+            default:
+                return null;
+        }
+
+    }
+}
--- run/LetterProcessor.java
+++ run/LetterProcessor.java
@@ -10,34 +10,52 @@ public class LetterProcessor{
         // Input layer size = 120, hidden layer = 60, output layer = 26
         int[] layerInfo = {120, 60, 26};
         NeuralNetwork neuralNetwork = new NeuralNetwork(layerInfo);
-        LetterProcessor imageProcessor = new LetterProcessor(neuralNetwork);
-        String filePath = System.getProperty("user.dir") + "/files/conjunto-dados/teste-x.txt";
-        imageProcessor.processImages(filePath);
-        neuralNetwork.printOutputs();
+        LetterProcessor letterProcessor = new LetterProcessor(neuralNetwork);
+        String filePath = System.getProperty("user.dir") + "/data/cross-validation/1-fold-x.txt";
+        letterProcessor.processImages(filePath);
     }
-
-    public LetterProcessor(NeuralNetwork neuralNetwork){
-        this.neuralNetwork = neuralNetwork;
-    }
-
+    
     public void processImages(String filePath){
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
             String line;
+            // para cada arquivo de fold - roda 130x para 130 linhas
+            int linha = 1;
+            double currentMSE = 0;
             while ((line = br.readLine()) != null){
+                linha = linha % 26;
+                linha = linha == 0 ? 26 : linha;
+                // System.out.println("Linha: " + linha);
+                double[] expectedOutputs = AlphabetVectors.getLetter(linha);
                 double[] inputs = parseInputLine(line);
                 double[] outputs = neuralNetwork.runFeedForward(inputs);
+                neuralNetwork.runBackpropagation(expectedOutputs, 0.5);
+                // neuralNetwork.printOutputs();
+                // for(double expectedOutput : expectedOutputs){
+                    //     System.out.println(expectedOutput);
+                    // }
+                    neuralNetwork.calculateMSE(expectedOutputs);
+                    currentMSE = neuralNetwork.getMSE();
+                    System.out.println("MSE: " + currentMSE);
+                // if(linha == 26){
+                    //     break;
+                    // }
+                    linha++;
+                }
+            } catch (IOException e){
+                e.printStackTrace();
             }
-        } catch (IOException e){
-            e.printStackTrace();
         }
-    }
+        
+        private double[] parseInputLine(String line){
+            String[] values = line.trim().split(",");
+            double[] inputs = new double[values.length];
+            for (int i = 0; i < values.length; i++){
+                inputs[i] = Double.parseDouble(values[i].trim());
+            }
+            return inputs;
+        }
 
-    private double[] parseInputLine(String line){
-        String[] values = line.trim().split(",");
-        double[] inputs = new double[values.length];
-        for (int i = 0; i < values.length; i++){
-            inputs[i] = Double.parseDouble(values[i].trim());
+        public LetterProcessor(NeuralNetwork neuralNetwork){
+            this.neuralNetwork = neuralNetwork;
         }
-        return inputs;
-    }
 }
