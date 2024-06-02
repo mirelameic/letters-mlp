c49cc06f03dc7c22272eb9cf5341be807cf7048c
mirelameic <mirelameic4@gmail.com>
2024-06-01 22:44:05 -0300

    wip trainning

--- run/LetterProcessor.java
+++ run/LetterProcessor.java
@@ -3,19 +3,39 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class LetterProcessor{
-
     private NeuralNetwork neuralNetwork;
 
     public static void main(String[] args){
         // Input layer size = 120, hidden layer = 60, output layer = 26
         int[] layerInfo = {120, 60, 26};
+        int[] folds = {2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int testFold = 1;
         NeuralNetwork neuralNetwork = new NeuralNetwork(layerInfo);
         LetterProcessor letterProcessor = new LetterProcessor(neuralNetwork);
-        String filePath = System.getProperty("user.dir") + "/data/cross-validation/1-fold-x.txt";
-        letterProcessor.processImages(filePath);
+        letterProcessor.crossValidation(folds, testFold);
+
+        // String filePath = System.getProperty("user.dir") + "/data/cross-validation/1-fold-x.txt";
+        // letterProcessor.processImages(filePath);
     }
-    
-    public void processImages(String filePath){
+
+    public void crossValidation(int folds[], int testFold){
+        for (int fold : folds){
+            // System.out.println("Fold: " + fold);
+            String filePath = setFilePathWithFoldNumber(fold);
+            processImages(filePath, false);
+        }
+
+
+        processImages(setFilePathWithFoldNumber(testFold), true);
+        // System.out.println(setFilePathWithFoldNumber(testFold));
+    }
+
+    public void processImages(String filePath, boolean isTestFold){
+        if (filePath == null || filePath.isEmpty()) {
+            System.err.println("File path is not set. Use setFilePathWithFoldNumber() to set the path to the fold file.");
+            return;
+        }
+
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
             String line;
             // para cada arquivo de fold - roda 130x para 130 linhas
@@ -28,7 +48,10 @@ public class LetterProcessor{
                 double[] expectedOutputs = AlphabetVectors.getLetter(linha);
                 double[] inputs = parseInputLine(line);
                 double[] outputs = neuralNetwork.runFeedForward(inputs);
-                neuralNetwork.runBackpropagation(expectedOutputs, 0.5);
+                if (!isTestFold){
+                    System.out.println("oi");
+                    neuralNetwork.runBackpropagation(expectedOutputs, 0.5);
+                }
                 // neuralNetwork.printOutputs();
                 // for(double expectedOutput : expectedOutputs){
                     //     System.out.println(expectedOutput);
@@ -44,6 +67,10 @@ public class LetterProcessor{
             } catch (IOException e){
                 e.printStackTrace();
             }
+        }
+
+        public String setFilePathWithFoldNumber(int foldNumber){
+            return System.getProperty("user.dir") + "/data/cross-validation/" + foldNumber + "-fold-x.txt";
         }
         
         private double[] parseInputLine(String line){
